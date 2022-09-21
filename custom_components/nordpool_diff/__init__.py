158fa829b4428fa859470c3e642a52471b20c951
dala318 <daniel@vikstrom.name>
2022-09-21 22:15:50 +0200

    Update __init__.py

--- custom_components/nordpool_diff/__init__.py
+++ custom_components/nordpool_diff/__init__.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import Config, HomeAssistant
 
-DOMAIN = "nordpool_diff"
+DOMAIN = "nordpool_planner"
 PLATFORMS = ["sensor"]
 
 
