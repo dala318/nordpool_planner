3f5874f65887fee58023a363193f144e4b1edfca
dala318 <daniel@vikstrom.name>
2024-09-04 09:44:07 +0000

    Try fixing test

--- tests/test_config_flow.py
+++ tests/test_config_flow.py
@@ -2,13 +2,35 @@
 
 from unittest import mock
 
+from config.custom_components.nordpool_planner.const import *
+from custom_components.nordpool_planner import config_flow
 import pytest
 from pytest_homeassistant_custom_component.async_mock import patch
+import voluptuous as vol
 
-from custom_components.nordpool_planner import config_flow
 from homeassistant import config_entries
+from homeassistant.helpers import selector
 
 
+NP_ENTITY_NAME = "sensor.nordpool_ent"
+
+SCHEMA_COPY = vol.Schema(
+    {
+        vol.Required(CONF_NAME): str,
+        vol.Required(CONF_TYPE): selector.SelectSelector(
+            selector.SelectSelectorConfig(options=CONF_TYPE_LIST),
+        ),
+        vol.Required(CONF_NP_ENTITY): selector.SelectSelector(
+            selector.SelectSelectorConfig(options=[NP_ENTITY_NAME]),
+        ),
+        vol.Required(CONF_ACCEPT_COST_ENTITY, default=False): bool,
+        vol.Required(CONF_ACCEPT_RATE_ENTITY, default=False): bool,
+        vol.Required(CONF_HIGH_COST_ENTITY, default=False): bool,
+    }
+)
+
+
+@pytest.mark.asyncio
 async def test_flow_init(hass):
     """Test the initial flow."""
     result = await hass.config_entries.flow.async_init(
@@ -16,7 +38,8 @@ async def test_flow_init(hass):
     )
 
     expected = {
-        "data_schema": config_flow.DATA_SCHEMA,
+        "data_schema": SCHEMA_COPY,
+        # "data_schema": config_flow.DATA_SCHEMA,
         "description_placeholders": None,
         "errors": {},
         "flow_id": mock.ANY,
