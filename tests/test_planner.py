ca39195181d986a2d5abda2addc51b7f59c8cdd4
dala318 <daniel@vikstrom.name>
2024-09-04 13:06:02 +0000

    A hail-mary attempt

--- tests/test_planner.py
+++ tests/test_planner.py
@@ -6,11 +6,18 @@ from custom_components.nordpool_planner import NordpoolPlanner
 import pytest
 
 # from pytest_homeassistant_custom_component.async_mock import patch
-import voluptuous as vol
+from pytest_homeassistant_custom_component.common import (
+    MockModule,
+    MockPlatform,
+    mock_integration,
+    mock_platform,
+)
 
 from custom_components.nordpool_planner.const import *
 from homeassistant import config_entries
-from homeassistant.helpers import selector
+from homeassistant.components import sensor
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 
 @pytest.mark.asyncio
@@ -23,6 +30,23 @@ async def test_planner_init(hass):
     SEARCH_LENGTH_ENT = "search_len"
     NP_ENT = "sensor.np_ent"
     CURRENCY = "EUR"
+
+    async def async_setup_entry_init(
+        hass: HomeAssistant, config_entry: config_entries.ConfigEntry
+    ) -> bool:
+        """Set up test config entry."""
+        await hass.config_entries.async_forward_entry_setups(
+            config_entry, [sensor.DOMAIN]
+        )
+        return True
+
+    mock_integration(
+        hass,
+        MockModule(
+            "nordpool",
+            async_setup_entry=async_setup_entry_init,
+        ),
+    )
 
     config_entry = config_entries.ConfigEntry(
         data={
@@ -39,6 +63,25 @@ async def test_planner_init(hass):
         source="user",
         title="Nordpool Planner",
         unique_id="123456",
+    )
+
+    # Unnamed binary sensor with device class but has_entity_name False -> no name
+    np_sensor = sensor.SensorEntity()
+    np_sensor.entity_id = NP_ENT
+    np_sensor._attr_device_class = sensor.SensorDeviceClass.MONETARY
+
+    async def async_setup_entry_platform(
+        hass: HomeAssistant,
+        config_entry: config_entries.ConfigEntry,
+        async_add_entities: AddEntitiesCallback,
+    ) -> None:
+        """Set up test sensor platform via config entry."""
+        async_add_entities([np_sensor])
+
+    mock_platform(
+        hass,
+        f"{"nordpool"}.{sensor.DOMAIN}",
+        MockPlatform(async_setup_entry=async_setup_entry_platform),
     )
 
     planner = NordpoolPlanner(hass, config_entry)
