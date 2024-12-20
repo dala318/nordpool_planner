3019d12bb34d9b7492afe6fe4b2c229c945699ab
DanielV <daniel@vikstrom.name>
2024-12-20 09:29:03 +0000

    Clean up diag data

--- custom_components/nordpool_planner/diagnostics.py
+++ custom_components/nordpool_planner/diagnostics.py
@@ -23,16 +23,8 @@ async def async_get_config_entry_diagnostics(
 ) -> dict[str, Any]:
     """Return diagnostics for a config entry."""
     diag_data = {
-        # "config_entry": async_redact_data(config_entry, TO_REDACT),
-        # "planner": async_redact_data(hass.data[DOMAIN][config_entry.entry_id], TO_REDACT),
-        "config_entry": config_entry,
+        # "config_entry": config_entry,  # Already included in the planner
         "planner": hass.data[DOMAIN][config_entry.entry_id],
     }
-    # planner: NordpoolPlanner = hass.data[DOMAIN][config_entry.entry_id]
-    # if planner is not None:
-    #     # diag_data["planner"] = planner.__dict__
-    #     # diag_data["prices"] = planner._prices_entity.__dict__
-    # else:
-    #     _LOGGER.warning("NordpoolPlanner is not available")
 
     return diag_data
